78ac3322b479399cb0ac73f79bee75ce45c1cd6e
adityanishad0317-rgb <adityanishad0317@gmail.com>
2026-08-08 17:47:55 +0530

    Update login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,1 +1,13 @@
-
+export async function onRequestPost(context) {
+  return new Response(
+    JSON.stringify({
+      success: true,
+      message: "Login endpoint is working."
+    }),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
+}
