6a00522cc3b199fe55fb2a98bec3b5b2cd46f9b6
adityanishad0317-rgb <adityanishad0317@gmail.com>
2026-08-09 11:17:05 +0530

    Update logout.js

--- functions/api/logout.js
+++ functions/api/logout.js
@@ -1,7 +1,8 @@
 const COOKIE_NAME = "ADMIN_SESSION";
 
 export async function onRequestGet(context) {
-  return new Response(
+
+  const response = new Response(
     JSON.stringify({
       success: true,
       message: "Logged out successfully."
@@ -9,9 +10,28 @@ export async function onRequestGet(context) {
     {
       status: 200,
       headers: {
-        "Content-Type": "application/json",
-        "Set-Cookie": `${COOKIE_NAME}=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0`
+        "Content-Type": "application/json"
       }
     }
   );
+
+  // Delete the current session cookie.
+  response.headers.append(
+    "Set-Cookie",
+    `${COOKIE_NAME}=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT`
+  );
+
+  // Also remove possible older cookies created with
+  // different paths.
+  response.headers.append(
+    "Set-Cookie",
+    `${COOKIE_NAME}=; Path=/admin; HttpOnly; Secure; SameSite=Strict; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT`
+  );
+
+  response.headers.append(
+    "Set-Cookie",
+    `${COOKIE_NAME}=; Path=/api; HttpOnly; Secure; SameSite=Strict; Max-Age=0; Expires=Thu, 01 Jan 1970 00:00:00 GMT`
+  );
+
+  return response;
 }
