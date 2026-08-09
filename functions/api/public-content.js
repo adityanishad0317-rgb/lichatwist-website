0373d01d17b4e4a8878b3ecbb500716db0d00001
adityanishad0317-rgb <adityanishad0317@gmail.com>
2026-08-09 17:28:10 +0530

    Update public-content.js
    Add project image fields to public content API

--- functions/api/public-content.js
+++ functions/api/public-content.js
@@ -49,15 +49,17 @@ projects_description,
 project1_category,
 project1_title,
 project1_description,
+project1_image,
 
 project2_category,
 project2_title,
 project2_description,
+project2_image,
 
 project3_category,
 project3_title,
-project3_description
-
+project3_description,
+project3_image
 FROM site_content
       WHERE id = 1
     `).first();
